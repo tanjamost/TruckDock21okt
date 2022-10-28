bbe3da6c91d0f30d1ef0e0669fa8bfd9f3e79992
tanjamost <105113462+tanjamost@users.noreply.github.com>
2022-10-28 17:09:44 +0200

    Update LitenLastbil.java

--- LitenLastbil.java
+++ LitenLastbil.java
@@ -1,19 +1,13 @@
 public class LitenLastbil  extends Vehicle {
     public LitenLastbil(double vikt) {
-        super(2);
+        this.typ = 2;
         this.vikt = vikt;
     }
 
-    @Override
-    public int getTyp() {
-        return typ;
-    }
+
     @Override
     public String toString() {
         return "Lätt lastbil, vikt = " + vikt;
     }
-    @Override
-    public double getVikt() {
-        return this.vikt;
-    }
+
 }
