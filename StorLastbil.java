08c3d636db1b1577cdf1faceb8ac3093b20ffc77
tanjamost <105113462+tanjamost@users.noreply.github.com>
2022-10-28 17:12:44 +0200

    Update StorLastbil.java

--- StorLastbil.java
+++ StorLastbil.java
@@ -1,20 +1,13 @@
 public class StorLastbil extends Vehicle {
     public StorLastbil(double vikt) {
-        super(3);
+        this.typ = 3;
         this.vikt = vikt;
     }
 
-    @Override
-    public int getTyp() {
-        return typ;
-    }
+
     @Override
     public String toString() {
         return "Tung lastbil, vikt = " + vikt;
     }
 
-    @Override
-    public double getVikt() {
-        return this.vikt;
-    }
 }
